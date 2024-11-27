1fa8588cc8023d1a53f39ddfa5aeb416f54bbed5
Leandro-Pinho <leandroessencia@gmail.com>
2024-11-27 09:06:07 -0300

    criando a função para adicionar

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const changeThemeBtn = document.querySelector('#change-theme')
+const inputText = document.getElementById('AddTask')
+const listContainer = document.querySelector('.list-container')
 
 changeThemeBtn.addEventListener('change', () => {
    document.body.classList.toggle('dark')
 })
+
+function AddTask(){
+    let li = document.createElement("li")
+    li.innerHTML = inputText.value
+    listContainer.appendChild(li)
+}
+
+inputText.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        if (inputText.value === '') {
+            alert("You must write something")
+        } else {
+            AddTask()
+        }
+      }
+    }
+)
+
